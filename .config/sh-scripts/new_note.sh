6171bbbac404e128910e6b1651d570461384c013
Oscar-Cabr <oscar.cabrerarojas@gmail.com>
2026-02-02 20:21:25 -0600

    update: Changed tmp notes to ~/Documents/Notes/ notes with timestamp

--- .config/sh-scripts/new_note.sh
+++ .config/sh-scripts/new_note.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-TEMPLATE="$HOME/Documents/Notas/template.fnx"
+NOTES_DIR="$HOME/Documents/Notas/"
+TEMPLATE="$NOTES_DIR/template.fnx"
 
-tmpfile="$(mktemp --suffix=.fnx)"
-cp "$TEMPLATE" "$tmpfile"
-feathernotes "$tmpfile"
+timestamp="$(date +"%Y-%m-%d_%H%M%S")"
+note_file="$NOTES_DIR/FeatherNote_$timestamp.fnx"
+
+cp "$TEMPLATE" "$note_file"
+feathernotes "$note_file"
